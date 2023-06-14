c8ef4b0926b7945c14a19dc56a0bf11013443d5b
Anais-RV <anaisrv@gmail.com>
2023-06-14 11:57:39 +0200

    adding notes for learning

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,13 @@
-function fizzbuzz(num) {
+function fizzbuzz(num) {  //  creo una función para saber si un número es divisible de 3 (Fizz), de 5(Buzz), de ambos(FizzBuzz) o de ninguno.
 
     if (num % 3 === 0 && num % 5 == 0){
-        return("FizzBuzz");
+        return("FizzBuzz"); // Si es divisible por ambos
     } else if (num % 3 === 0){
-        return("Fizz");
+        return("Fizz"); // Si es divisible por 3
     } else if (num % 5 === 0){
-        return "Buzz";
+        return "Buzz";  // Si es divisible por 5
     } else{
-        return num.toString();
+        return num.toString(); // Sino devuelve un string con num
     }
 }
-module.exports = fizzbuzz;
+module.exports = fizzbuzz; //exporto el modulo (node.js)
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,10 +1,13 @@
-const fizzbuzz = require("./fizzbuzz");
+const fizzbuzz = require("./fizzbuzz");  // importamos la función
 
-test('Should return "Fizz" when the num divisible by 3', () => {
-// Arrange
-const num = 9;
-// Act
-const result = fizzbuzz(num);
-// Assert
-expect(result).toBe('Fizz');
+test('Should return "Fizz" when num divisible by 3', () => { //arrow function 
+
+    // Arrange - definimos el escenario de prueba
+    const num = 9;
+
+    // Act - llamamos a la función, la operativa
+    const result = fizzbuzz(num);
+
+    // Assert - Indicamos lo esperado para determinar si la prueba es exitosa
+    expect(result).toBe('Fizz');
 });
